9b05bc9c1234ca13e5e06e764d687dc50197b1cd
Marcelo Bentivoglio <mabj90s@gmail.com>
2018-05-03 15:43:24 -0300

    Added Client and User a relationship of has_one and belongs_to + signupf

--- db/migrate/20180430220935_add_belongs_to_user_from_client.rb
+++ db/migrate/20180430220935_add_belongs_to_user_from_client.rb
@@ -1,5 +1,5 @@
 class AddBelongsToUserFromClient < ActiveRecord::Migration[5.1]
   def change
-    add_reference(:clients, :users, foreign_key: true)
+    add_reference(:clients, :users, index: { unique: true }, foreign_key: true)
   end
 end
