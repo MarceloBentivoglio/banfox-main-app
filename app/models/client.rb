6ceae5dcd1623204cd3f0d03e508199b3d924143
Joaquim Oliveira <joaquim.oliveira.nt@gmail.com>
2018-05-10 19:00:40 -0300

    wizard with wicked begin

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,6 +1,7 @@
 class Client < ApplicationRecord
-  validates :company_name, presence: true
-  validates :full_name, presence: true
-  validates :cpf, presence: true
-  validates :cnpj, presence: true
+  # validates :company_name, presence: true
+  # validates :full_name, presence: true
+  # validates :cpf, presence: true
+  # validates :cnpj, presence: true
+  has_many :users
 end
