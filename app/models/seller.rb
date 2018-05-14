148e47a3c22e439834d75688e93162c81805891d
Joaquim Oliveira <joaquim.oliveira.nt@gmail.com>
2018-05-14 16:24:25 -0300

    first try to make it work

--- app/models/seller.rb
+++ app/models/seller.rb
@@ -1,9 +1,12 @@
 class Seller < ApplicationRecord
+  PURPOSE = ["product_manufacture", "service_provision", "product_reselling"]
+
   # validates :company_name, presence: true
   # validates :full_name, presence: true
   # validates :cpf, presence: true
   # validates :cnpj, presence: true
   has_many :users
+
 
   # We need this to upload the invoices in xml format and the cheques in pdf
   # has_attached_file :invoice_document
