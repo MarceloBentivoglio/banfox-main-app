017b5249909cb1a417b4f3ba00802773b36ee72d
Joaquim Oliveira <joaquim.oliveira.nt@gmail.com>
2018-05-14 18:20:50 -0300

    tag working

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,3 @@
 import "bootstrap";
+import { selectTagOnClick } from "../components/wizard";
+selectTagOnClick();
---
+++ test/models/company_purpose_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class CompanyPurposeTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
