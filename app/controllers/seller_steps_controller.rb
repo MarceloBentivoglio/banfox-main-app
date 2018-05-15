0a8470f01dbb199849f49752d54961f1a374da18
Joaquim Oliveira <joaquim.oliveira.nt@gmail.com>
2018-05-15 17:21:14 -0300

    tags that show type of company are working. Now we have two types of tags, one that you can select multiple tags and one that you can select only one

--- app/controllers/seller_steps_controller.rb
+++ app/controllers/seller_steps_controller.rb
@@ -34,7 +34,9 @@ class SellerStepsController < ApplicationController
   private
 
   def seller_params
-      params.require(:seller).permit(:full_name, :cpf, :phone, :company_name, :cnpj, :product_manufacture, :service_provision, :product_reselling)
+      params.require(:seller).permit(:full_name, :cpf, :phone, :company_name,
+      :cnpj, :product_manufacture, :service_provision, :product_reselling,
+      :company_type )
   end
 end
 
