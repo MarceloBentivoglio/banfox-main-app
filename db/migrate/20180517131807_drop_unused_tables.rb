5a620cfb9cc6b76b24aa6f99ac8c0313f9d3c255
Joaquim Oliveira <joaquim.oliveira.nt@gmail.com>
2018-05-17 19:23:42 -0300

    XML uploading and creating the invoice is working

--- db/migrate/20180517131807_drop_unused_tables.rb
+++
@@ -1,6 +0,0 @@
-class DropUnusedTables < ActiveRecord::Migration[5.1]
-  def change
-    drop_table :company_purposes
-    drop_table :purposes
-  end
-end
--- db/migrate/20180517132646_add_payer_to_invoice.rb
+++
@@ -1,5 +0,0 @@
-class AddPayerToInvoice < ActiveRecord::Migration[5.1]
-  def change
-    add_reference :invoices, :payer, foreign_key: true
-  end
-end
