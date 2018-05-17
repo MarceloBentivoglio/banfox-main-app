110e06088db2b7d4585f03aefed304dc916dfe34
Joaquim Oliveira <joaquim.oliveira.nt@gmail.com>
2018-05-17 10:54:45 -0300

    Invoice Installment and Payer created. Money installed. Beginning to work with XML

--- app/models/seller.rb
+++ app/models/seller.rb
@@ -24,7 +24,6 @@ class Seller < ApplicationRecord
     "pay_90_plus" => "Parcelado em 90+",
   }
 
-
   CLIENT_OPTIONS = {
     "pay_factoring" => "Aceita pagar factoring",
     "permit_contact_client" => "Permito contactar meu cliente",
@@ -35,7 +34,6 @@ class Seller < ApplicationRecord
     "consent" => "Li e aceito os termos do site"
   }
 
-
   enum company_type: {
     LTDA: 0,
     SA: 1,
@@ -43,15 +41,7 @@ class Seller < ApplicationRecord
     MEI: 3,
   }
 
-  # validates :company_name, presence: true
-  # validates :full_name, presence: true
-  # validates :cpf, presence: true
-  # validates :cnpj, presence: true
   has_many :users
-  has_many :company_purposes
-  has_many :purposes, through: :company_purposes
+  has_many :invoices
 
-
-  # We need this to upload the invoices in xml format and the cheques in pdf
-  # has_attached_file :invoice_document
 end
