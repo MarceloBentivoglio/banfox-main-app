4a6279120cdb35e79c47afd2bdee3d4093cd03c7
Joaquim Oliveira <joaquim.oliveira.nt@gmail.com>
2018-05-18 15:03:44 -0300

    preparared to start creating the validation state machine

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   belongs_to :seller, optional: true
 
+  def registration_completed?
+    seller.consent
+  end
 end
