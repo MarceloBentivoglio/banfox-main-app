e5da2dd86c861e236e19213bf67bccff68969dc8
Joaquim Oliveira <joaquim.oliveira.nt@gmail.com>
2018-05-22 19:19:04 -0300

    Form mask working

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,7 @@
 import "bootstrap";
 import { selectTagOnClick } from "../components/wizard";
 import { sliderShowValue } from "../components/slider";
+import { maskCpfAndCnpj } from "../components/formMask";
 selectTagOnClick();
 sliderShowValue();
+maskCpfAndCnpj();
