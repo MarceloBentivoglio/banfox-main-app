14c5c23eb9568c5b57d6bdf3e48f54040533d108
Joaquim Oliveira <joaquim.oliveira.nt@gmail.com>
2018-05-24 18:19:40 -0300

    all validations are working. We did not put the validation messagens for AtLeastOne validations on the views however it is still validating it.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   belongs_to :seller, optional: true
+  validates :email, presence: true, email: true
 end
