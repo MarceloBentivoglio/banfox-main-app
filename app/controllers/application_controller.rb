4a2d3d40e07a6e3b51a1a86b809d7af89d4c91c5
Joaquim Oliveira <joaquim.oliveira.nt@gmail.com>
2018-05-30 18:46:40 -0300

    Now we can make an upload of any kind of documents for the registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
   before_action :authenticate_user!
+  before_action :require_active
   helper_method :resource_name, :resource, :devise_mapping, :resource_class
 
   def after_sign_in_path_for(resource_or_scope)
@@ -21,5 +23,19 @@ class ApplicationController < ActionController::Base
 
   def devise_mapping
     @devise_mapping ||= Devise.mappings[:user]
+  end
+
+  private
+  # TODO: refactor, I am sure that there is a smater way to write this code with less querries
+  def require_active
+    if (seller = current_user.seller)
+      unless seller.active?
+        flash[:error] = "Você precisa completar seu cadastro"
+        redirect_to "#{seller_steps_path}/#{seller.next_step}"
+      end
+    else
+      flash[:error] = "Você precisa completar seu cadastro"
+      redirect_to seller_steps_path
+    end
   end
 end
