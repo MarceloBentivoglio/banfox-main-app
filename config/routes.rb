20237ad8330147fa3d66e653cfbc4fbbba5168f6
Joaquim Oliveira <joaquim.oliveira.nt@gmail.com>
2018-05-30 14:05:11 -0300

    Now I can uplaod an image and show it

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-
   mount ForestLiana::Engine => '/forest'
-  get 'invoices/index'
-  get 'invoices/new'
   root to: 'pages#home'
   get "pages/howitworks", to: "pages#howitworks"
   devise_for :users, controllers: { registrations: "registrations" }
   resources :seller_steps
   resources :invoices, only: [:index, :new, :create, :destroy]
+  resources :documents, only: [:index, :new, :create, :destroy]
 end
