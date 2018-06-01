d786e7c8ac9fe3f6bbaa5be8f6c4db532fd9aa67
Joaquim Oliveira <joaquim.oliveira.nt@gmail.com>
2018-06-01 10:39:22 -0300

    log_out, sign_in and sign_up working good

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   mount ForestLiana::Engine => '/forest'
   root to: 'pages#home'
   get "pages/howitworks", to: "pages#howitworks"
-  devise_for :users, controllers: { registrations: "registrations" }
+  devise_for :users, controllers: {
+    registrations: "users/registrations",
+    sessions: 'users/sessions',
+     }
   resources :seller_steps
   resources :invoices, only: [:index, :new, :create, :destroy]
   resources :documents, only: [:index, :new, :create, :destroy]
