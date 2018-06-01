d4da6d92d65d7cc0b41ea819faa79f81505f33a9
Joaquim Oliveira <joaquim.oliveira.nt@gmail.com>
2018-06-01 10:57:39 -0300

    now log in log out and erverthing else is workin

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: "users/registrations",
     sessions: 'users/sessions',
-     }
+    confirmations: 'users/confirmations',
+    passwords: 'users/passwords',
+    unlocks: 'users/unlocks',
+  }
   resources :seller_steps
   resources :invoices, only: [:index, :new, :create, :destroy]
   resources :documents, only: [:index, :new, :create, :destroy]
