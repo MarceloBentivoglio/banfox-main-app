9261fb49d2fa862c0c077bc26d11f9f07f2447e1
Joaquim Oliveira <joaquim.oliveira.nt@gmail.com>
2018-06-11 18:54:51 -0300

    now all the querries are working just fine if you pass the seller for it.

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -42,19 +42,20 @@ class Invoice < ApplicationRecord
     end
   end
 
-  # Verificar se essa lógica está certa ou se eu teria que colocar um seller_invoices antes de approved
+  # TODO: Verificar se essa lógica está certa ou se eu teria que colocar um seller_invoices antes de approved
   def self.in_store(seller_invoices)
     seller_invoices ||= self
     seller_invoices.registred.or(approved)
   end
 
-  def self.overdue(seller_invoices)
-    seller_invoices.find_by_sql("
+  # TODO: transform all sql on Active Record
+  def self.overdue(seller)
+    find_by_sql(["
       SELECT a.* FROM invoices a
       JOIN installments b ON a.id = b.invoice_id
-      WHERE a.backoffice_status = 2
+      WHERE a.backoffice_status = 2 AND a.seller_id = ?
       GROUP BY a.id
-      HAVING SUM(CASE WHEN (liquidation_status = 0) AND (due_date <= NOW()) THEN 1 ELSE 0 END) > 0 ")
+      HAVING SUM(CASE WHEN (liquidation_status = 0) AND (due_date <= NOW()) THEN 1 ELSE 0 END) > 0", seller.id])
   end
   # NOTE: the upper querry performers better than the one below! But will let this one just in case
   def self.overdue_2(seller_invoices)
@@ -62,23 +63,23 @@ class Invoice < ApplicationRecord
     date_range_past = (Date.today - 6.years)...(Date.today)
     seller_invoices.deposited.joins(:installments).where(installments: {liquidation_status: "open", due_date: date_range_past})
   end
-  # TODO: transform on Active Record
-  def self.opened(seller_invoices)
-    seller_invoices.find_by_sql("
+
+  def self.opened(seller)
+    find_by_sql(["
       SELECT a.* FROM invoices a
       JOIN installments b ON a.id = b.invoice_id
-      WHERE a.backoffice_status = 2
+      WHERE a.backoffice_status = 2 AND a.seller_id = ?
       GROUP BY a.id
-      HAVING SUM(CASE WHEN (liquidation_status = 0) AND (due_date <= NOW()) THEN 1 ELSE 0 END) < 1 AND SUM(CASE liquidation_status WHEN 1 THEN 1 ELSE 0 END) < COUNT(liquidation_status) ")
+      HAVING SUM(CASE WHEN (liquidation_status = 0) AND (due_date <= NOW()) THEN 1 ELSE 0 END) < 1 AND SUM(CASE liquidation_status WHEN 1 THEN 1 ELSE 0 END) < COUNT(liquidation_status)", seller.id])
   end
-  # TODO: transform on Active Record
-  def self.paid(seller_invoices)
-    seller_invoices.find_by_sql("
+
+  def self.paid(seller)
+    find_by_sql(["
       SELECT a.* FROM invoices a
       JOIN installments b ON a.id = b.invoice_id
-      WHERE a.backoffice_status = 2
+      WHERE a.backoffice_status = 2 AND a.seller_id = ?
       GROUP BY a.id
-      HAVING SUM(CASE liquidation_status WHEN 1 THEN 1 ELSE 0 END) = COUNT(liquidation_status) ")
+      HAVING SUM(CASE liquidation_status WHEN 1 THEN 1 ELSE 0 END) = COUNT(liquidation_status) ", seller.id])
   end
   # NOTE: the upper querry performers better than the one below! But will let this one just in case
   def self.paid_2(seller_invoices)
@@ -88,13 +89,13 @@ class Invoice < ApplicationRecord
     }
   end
 
-  def self.rebought(seller_invoices)
-    seller_invoices.find_by_sql("
+  def self.rebought(seller)
+    find_by_sql(["
       SELECT a.* FROM invoices a
       JOIN installments b ON a.id = b.invoice_id
-      WHERE a.backoffice_status = 2
+      WHERE a.backoffice_status = 2 AND a.seller_id = ?
       GROUP BY a.id
-      HAVING SUM(CASE liquidation_status WHEN 2 THEN 1 ELSE 0 END) = COUNT(liquidation_status) ")
+      HAVING SUM(CASE liquidation_status WHEN 2 THEN 1 ELSE 0 END) = COUNT(liquidation_status)", seller.id])
   end
   # NOTE: the upper querry performers better than the one below! But will let this one just in case
   def self.rebought_2(seller_invoices)
@@ -104,12 +105,12 @@ class Invoice < ApplicationRecord
     }
   end
 
-  def self.lost
-    seller_invoices.find_by_sql("
+  def self.lost(seller)
+    find_by_sql(["
       SELECT a.* FROM invoices a
       JOIN installments b ON a.id = b.invoice_id
-      WHERE a.backoffice_status = 2
+      WHERE a.backoffice_status = 2 AND a.seller_id = ?
       GROUP BY a.id
-      HAVING SUM(CASE liquidation_status WHEN 3 THEN 1 ELSE 0 END) = COUNT(liquidation_status) ")
+      HAVING SUM(CASE liquidation_status WHEN 3 THEN 1 ELSE 0 END) = COUNT(liquidation_status)", seller.id])
   end
 end
