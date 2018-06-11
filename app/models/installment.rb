8f5b6b84001f9a897b24f2c07752a4d5850fe08b
Joaquim Oliveira <joaquim.oliveira.nt@gmail.com>
2018-06-11 17:32:09 -0300

    Querries funcionando

--- app/models/installment.rb
+++ app/models/installment.rb
@@ -2,6 +2,7 @@ class Installment < ApplicationRecord
   belongs_to :invoice, optional: true
   monetize :value_cents, with_model_currency: :currency
 
+# Any change in this enum must be reflected on the sql querry in Invoice Model
   enum liquidation_status: {
     open: 0,
     paid: 1,
