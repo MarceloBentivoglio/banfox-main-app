40d3ab96429c92b009b349dd0dfbd18eb6723b16
Joaquim Oliveira <joaquim.oliveira.nt@gmail.com>
2018-06-13 12:22:08 -0300

    Now you can see the invoices and its installments. Next steps delete an invoice from store

--- app/models/installment.rb
+++ app/models/installment.rb
@@ -9,4 +9,35 @@ class Installment < ApplicationRecord
     rebought: 2,
     pdd: 3,
   }
+
+  INSTALLMENT_STATUS = [
+    "A vencer",
+    "Vence hoje",
+    "Em atraso",
+    "Paga",
+    "Recomprada",
+    "Perdida",
+  ]
+
+  def status
+    return INSTALLMENT_STATUS[0] if on_date?
+    return INSTALLMENT_STATUS[1] if due_today?
+    return INSTALLMENT_STATUS[2] if overdue?
+    return INSTALLMENT_STATUS[3] if paid?
+    return INSTALLMENT_STATUS[4] if rebought?
+    return INSTALLMENT_STATUS[5] if pdd?
+
+  end
+
+  def overdue?
+    open? && due_date < Date.today
+  end
+
+  def on_date?
+    open? && due_date > Date.today
+  end
+
+  def due_today?
+    open? && due_date == Date.today
+  end
 end
