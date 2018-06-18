739e86179dc806dea41d3b11f612960478647f48
Joaquim Oliveira <joaquim.oliveira.nt@gmail.com>
2018-06-18 17:39:41 -0300

    putted all the bucketeers secrets inside the credentials and also used Rails.env.to_sym in the Rails.application.credentials to avoid doing if development and if production

--- config/initializers/forest_liana.rb
+++ config/initializers/forest_liana.rb
@@ -1,4 +1,2 @@
-ForestLiana.env_secret = Rails.application.credentials.development[:forest_env_secret] if Rails.env.development?
-ForestLiana.env_secret = Rails.application.credentials.production[:forest_env_secret] if Rails.env.production?
-ForestLiana.auth_secret = Rails.application.credentials.development[:forest_auth_secret] if Rails.env.development?
-ForestLiana.auth_secret = Rails.application.credentials.production[:forest_auth_secret] if Rails.env.production?
+ForestLiana.env_secret = Rails.application.credentials[Rails.env.to_sym][:forest_env_secret]
+ForestLiana.auth_secret = Rails.application.credentials[Rails.env.to_sym][:forest_auth_secret]
