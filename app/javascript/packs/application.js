b47db530404809866f3836c3f3bf561196354d3e
Joaquim Oliveira <joaquim.oliveira.nt@gmail.com>
2018-06-20 11:35:12 -0300

    javascript that constains the logic to name the files that we are wanting to upload is now in webpack

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,8 @@ import "bootstrap";
 import { selectTagOnClick } from "../components/wizard";
 import { sliderShowValue } from "../components/slider";
 import { maskCpfAndCnpj } from "../components/formMask";
+import { nameFilesToUpload } from "../components/filesUpload";
 selectTagOnClick();
 sliderShowValue();
 maskCpfAndCnpj();
+nameFilesToUpload();
