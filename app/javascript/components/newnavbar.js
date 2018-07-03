2a577a0b6e78e87fa57bdc18794d2f988f4bebaa
Marcelo Bentivoglio <mabj90s@gmail.com>
2018-07-03 11:33:17 -0300

    first draft of this session done

--- app/javascript/components/newnavbar.js
+++ app/javascript/components/newnavbar.js
@@ -6,11 +6,11 @@ function animateNavbar() {
       if ($(this).scrollTop() > 150) {
         $(document.getElementById('logo-banner')).addClass('logo-navbar-dark');
         $(document.getElementById('logo-banner')).removeClass('logo-navbar');
-        $(document.getElementById('new-homenavbar-mvp')).addClass('white-navbar');
+        $(document.getElementById('homenavbar-mvp')).addClass('white-navbar');
       } else {
         $(document.getElementById('logo-banner')).addClass('logo-navbar');
         $(document.getElementById('logo-banner')).removeClass('logo-navbar-dark');
-        $(document.getElementById('new-homenavbar-mvp')).removeClass('white-navbar');
+        $(document.getElementById('homenavbar-mvp')).removeClass('white-navbar');
       }
     });
   });
