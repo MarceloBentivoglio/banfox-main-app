b01a70a0db0008c0b2de486340c88e9fb500ea2b
Marcelo Bentivoglio <mabj90s@gmail.com>
2018-07-04 18:22:40 -0300

    updated login and signup layouts

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,6 +3,7 @@
 class Users::SessionsController < Devise::SessionsController
   skip_before_action :require_active
   # before_action :configure_sign_in_params, only: [:create]
+  layout "sessions_layout"
 
   # GET /resource/sign_in
   # def new
