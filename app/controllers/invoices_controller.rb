b2f3536ab0f8372453c17d701d8607bca99f94cc
Joaquim Oliveira <joaquim.oliveira.nt@gmail.com>
2018-07-05 15:17:01 -0300

    Flashes are now presented when someone tries to upload something that is not an xml or an invoice that doesn't have the same CNPJ of the seller

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,5 +1,4 @@
 class InvoicesController < ApplicationController
-  include ApplicationHelper
   before_action :set_seller, only: [:store, :opened, :history, :show, :new, :create]
 
   def store
@@ -21,14 +20,13 @@ class InvoicesController < ApplicationController
   def create
     if params[:invoice]
       extract = ExtractDataFromXml.new
-      show_message = false
-      messages = []
+      flash[:alert] = []
       invoices = extract.invoice(params[:invoice][:xmls], @seller)
       operation = Operation.create
       invoices.each do |invoice|
         if invoice.instance_of?(RuntimeError)
-          messages << invoice.message
-          show_message = true
+          flash[:alert] << 'Uma das notas que você subiu contem um CNPJ que não confere com o seu. As demais notas (caso haja) foram adicionadas' if invoice.message == "Invoice do not belongs to seller"
+          flash[:alert] << 'Um dos arquivos que você subiu não é um XML' if invoice.message == "File is not a xml type"
         else
           invoice.operation = operation
           invoice.save!
@@ -36,14 +34,6 @@ class InvoicesController < ApplicationController
         end
       end
       operation.destroy if operation.invoices.empty?
-      if show_message
-        messages.each do |err_message|
-          flash_message(:alert, 'Uma das notas que você subiu contem um CNPJ que não confere com o seu. As demais notas (caso haja) foram adicionadas') if err_message == "Invoice do not belongs to seller"
-          flash_message(:alert, 'Um dos arquivos que você subiu não é um XML') if err_message == "File is not a xml type"
-          # flash[:alert] << "Uma das notas que você subiu contem um CNPJ que não confere com o seu. As demais notas (caso haja) foram adicionadas." if err_message == "Invoice do not belongs to seller"
-          # flash[:alert] << "Um dos arquivos que você subiu não é um XML" if err_message == "File is not a xml type"
-        end
-      end
       redirect_to store_invoices_path
     else
       flash[:alert] = "É necessário ao menos subir uma nota fiscal em XML"
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,3 @@
 module ApplicationHelper
 
-  def flash_message(type, text)
-    flash[type] ||= []
-    flash[type] << text
-  end
 end
