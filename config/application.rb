71c2fef6ab8fe2507d10611eecfbc643a3456fe9
Joaquim Oliveira <joaquim.oliveira.nt@gmail.com>
2018-07-12 12:04:46 -0300

    Brasilia time zone set for rails and for active record

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,9 @@ module Mvpinvest
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    #JON: Setting time zone
+    config.time_zone = 'Brasilia'
+    config.active_record.default_timezone = :local
   end
 end
