5c43741ac32b6b10b9d225db78a939e8808d4fec
Joaquim Oliveira <joaquim.oliveira.nt@gmail.com>
2018-07-20 18:14:44 -0300

    Something is wrong in the css loading that started after I created the progressbar and did yarn install. I will search in the diferences

---
+++ app/javascript/components/mvpProgressbar.js
@@ -0,0 +1,6 @@
+const displayProgressbar = () => {
+  const value = document.getElementById('mvp-progressbar').getAttribute("data-value");
+  console.log(value);
+};
+
+export { displayProgressbar };
