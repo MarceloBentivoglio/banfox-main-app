5dc3fb49b9bdf0105b93652a998792321f8ebe3e
Joaquim Oliveira <joaquim.oliveira.nt@gmail.com>
2018-08-22 16:31:34 -0300

    created one alternative for the phone mask to receive mobile and landline. Not beautiful, but works

--- app/javascript/components/formMask.js
+++ app/javascript/components/formMask.js
@@ -10,23 +10,11 @@ const currencyOptions = {
         thousandsSeparator: '.'
 };
 
-const phoneOptions =  {
-  onKeyPress: function(phone, e, field, options) {
-    console.log(e);
-    console.log(phone.length)
-    const masks = ['(00) 9 0000-0000', '(00) 0000-0000'];
-    let mask = (phone.length>14) ? masks[0] : masks[1];
-    if (localStorage['mobile'] == 'true') {mask = masks[0]};
-    $('.phone').mask(mask, phoneOptions);
-    localStorage['mobile'] = (phone.length < 14) ? false : true;
-}};
-
 function maskCpfCnpjCurrency() {
   $('.cpf').mask('000.000.000-00');
   $('.cnpj').mask('00.000.000/0000-00');
   $('.mobile').mask('(00) 9 0000-0000');
-  // $('.phone').mask('(00) 0000-0000');
-  $('.phone').mask('(00) 0000-0000', phoneOptions);
+  $('.phone').mask('(00) 000000000');
   $('.birth_date').mask('00/00/0000');
   $('.zip_code').mask('00000-000');
   if (document.getElementById("monthly_revenue")) {
