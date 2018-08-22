1725f99a278e718749b69bbef914b9f8e752d364
Joaquim Oliveira <joaquim.oliveira.nt@gmail.com>
2018-08-22 16:44:51 -0300

    reseted the DB so you need to use db:drop db:create db:migratee

--- db/migrate/20180429220754_create_sellers.rb
+++ db/migrate/20180429220754_create_sellers.rb
@@ -3,10 +3,19 @@ class CreateSellers < ActiveRecord::Migration[5.1]
     create_table :sellers do |t|
       t.string :full_name
       t.string :cpf
+      t.string :birth_date
       t.string :phone
       t.string :company_name
       t.string :company_nickname
       t.string :cnpj
+      t.string :website
+      t.string :address
+      t.string :address_number
+      t.string :address_comp
+      t.string :neighborhood
+      t.string :state
+      t.string :city
+      t.string :zip_code
       t.string :inscr_est
       t.string :inscr_mun
       t.string :nire
@@ -17,13 +26,12 @@ class CreateSellers < ActiveRecord::Migration[5.1]
       t.bigint :monthly_units_sold
       t.monetize :cost_per_unit
       t.monetize :debt
-      t.string :address
-      t.string :address_number
-      t.string :address_comp
-      t.string :neighborhood
-      t.string :state
-      t.string :city
-      t.string :zip_code
+      t.string :full_name_partner
+      t.string :cpf_partner
+      t.string :birth_date_partner
+      t.string :mobile
+      t.string :mobile_partner
+      t.string :email_partner
       t.boolean :consent
       t.integer :validation_status
       t.integer :analysis_status, default: 0
