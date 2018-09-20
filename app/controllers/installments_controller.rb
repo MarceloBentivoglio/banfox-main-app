08f8ebb01c73bf95cdf282139c5cb85645f39458
Joaquim Oliveira <joaquim.oliveira.nt@gmail.com>
2018-09-20 18:57:09 -0300

    new status on the status bottom bar are working

--- app/controllers/installments_controller.rb
+++ app/controllers/installments_controller.rb
@@ -2,9 +2,19 @@ class InstallmentsController < ApplicationController
   before_action :set_seller, only: [:store, :opened, :history]
 
   def store
+    @no_operation_in_analysis        = true
+    @operation_analysis_finished     = false
+    @operation_completely_approved   = false
+    @operation_completely_rejected   = false
+    operation = Operation.last_from_seller(@seller)
     @installments = Installment.ordered_in_analysis(@seller).paginate(page: params[:page])
-    @no_operation_in_analysis = false
-    if @installments.empty?
+    @no_operation_in_analysis        = false
+    if operation && operation.analysis_finished?
+      @installments = operation.installments.paginate(page: params[:page])
+      @operation_analysis_finished   = true
+      @operation_completely_approved = true if operation.completely_approved?
+      @operation_completely_rejected = true if operation.completely_rejected?
+    elsif @installments.empty?
       @installments = Installment.in_store(@seller).paginate(page: params[:page])
       @no_operation_in_analysis = true
     end
--- app/models/operation.rb
+++ app/models/operation.rb
@@ -2,4 +2,18 @@ class Operation < ApplicationRecord
   has_many :installments
   has_many :rebuys, dependent: :destroy
   validates :consent, acceptance: {message: "é necessário dar o seu aval para a operação"}
+  default_scope { order(created_at: :asc) }
+  scope :last_from_seller, -> (seller) { joins(installments: [invoice: [:seller]]).where("sellers.id": seller.id).distinct.last }
+
+  def analysis_finished?
+    installments.all? { |i| i.approved? || i.rejected?}
+  end
+
+  def completely_approved?
+    installments.all? { |i| i.approved? }
+  end
+
+  def completely_rejected?
+    installments.all? { |i| i.rejected? }
+  end
 end
