d2716b32d8cac96ae5b60c37d2fc425b59170970
Joaquim Oliveira <joaquim.oliveira.nt@gmail.com>
2019-04-24 16:54:55 -0300

    Corrected the concern that cleans up the users inputs and downsizes it

--- app/models/concerns/user_input_processing.rb
+++ app/models/concerns/user_input_processing.rb
@@ -3,13 +3,13 @@ module UserInputProcessing
 
   def downcase_words
     %i{full_name company_name company_nickname website address address_number address_comp neighborhood state city nire full_name_partner email_partner email}.each do |attr|
-      self.__send__(attr).downcase! unless self.__send__(attr).nil?
+      self.__send__(attr).downcase! unless self.try(attr).nil?
     end
   end
 
    def clean_inputs
     %i{mobile mobile_partner phone zip_code phone cpf cpf_partner cnpj birth_date birth_date_partner}.each do |attr|
-      self.__send__(attr).gsub!(/[^0-9A-Za-z]/, '') unless self.__send__(attr).nil?
+      self.__send__(attr).gsub!(/[^0-9A-Za-z]/, '') unless self.try(attr).nil?
     end
   end
 
