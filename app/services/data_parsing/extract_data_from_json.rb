0984b671f3fb7627f4adbf70e6868bec409fec54
Joaquim Oliveira <joaquim.oliveira.nt@gmail.com>
2019-05-09 17:07:39 -0300

    created a seed for an admin in production and changed the json parsing rule

--- app/services/data_parsing/extract_data_from_json.rb
+++ app/services/data_parsing/extract_data_from_json.rb
@@ -1,28 +1,9 @@
 class ExtractDataFromJson
   attr_reader :invoice
 
-  DATA_TEMPLATE = {
-    # "id" : "2a73f08c34da2caa8902f5545b9624db",
-    "authenticity_code" => "343T.8671.0842.2932099-S",
-    "number" => "000245",
-    "issue_date" => "2019-02-05",
-    "total_value" => "4878900",
-    "days_until_due" => "21",
-    "payer_cnpj" => "09138393000179",
-    "payer_name" => "GME AEROSPACE INDUSTRIA DE MATERIAL COMPOSTO S.A",
-    "payer_address" => "ALAMEDA BOM PASTOR, 1683",
-    "payer_neighborhood" => "OURO FINO",
-    "payer_city" => "SAO JOSE DOS PINHAIS",
-    "payer_state" => "PR",
-    "payer_zipcode" => "83015140",
-    "payer_email" => "eugenia.lopes@gmebrasil.com.br",
-    "seller_cnpj" => "15028515000177"
-  }
-
   def initialize(invoice_data)
     @data = invoice_data
     @invoice = Invoice.find_by_doc_parser_ref(@data["document_id"])
-    set_template_value_to_empty_keys
     unless @invoice.seller
       @invoice.seller = Seller.find_by_cnpj(@data["seller_cnpj"])
     end
@@ -36,7 +17,7 @@ class ExtractDataFromJson
     @i = Installment.new()
     ninety_days = 90.days.since.to_date
     @i.value = Money.new(@data["total_value"])
-    @i.number = "#{@data["number"]}/01"
+    @i.number = "01"
     @i.due_date = Date.parse(@data["issue_date"]) + @data["days_until_due"].to_i.days
     @i.backoffice_status = ((@i.due_date <= Date.current) || (@i.due_date > ninety_days)) ? :unavailable : :available
     @i.unavailability = set_unavailability(@i.due_date, ninety_days)
@@ -71,16 +52,9 @@ class ExtractDataFromJson
   def set_unavailability (due_date, ninety_days)
     return :due_date_past if due_date <= Date.current
     return :due_date_later_than_limit if due_date > ninety_days
-    return :non_applicable
+    return :unavailability_non_applicable
   end
 
-  def set_template_value_to_empty_keys
-    @data.each do |key, value|
-       if value.empty?
-          @data[key] = DATA_TEMPLATE[key]
-       end
-    end
-  end
 end
 
 
