bee085d42b95bdabb836a41150d294fb46dfd559
Ricardo Sasaki <sasaki@MacBook-Air-de-Ricardo.local>
2019-08-06 18:20:48 -0300

    added PartnerSerasa Pipeline to recurrent_operation service

--- app/models/risk/service/recurrent_operation_strategy.rb
+++ app/models/risk/service/recurrent_operation_strategy.rb
@@ -2,7 +2,8 @@ module Risk
   module Service
     class RecurrentOperationStrategy < Risk::Service::Operation
       #Interface Enforcement
-      pipeline_list  Risk::Pipeline::Serasa
+      pipeline_list  Risk::Pipeline::Serasa,
+                     Risk::Pipeline::PartnerSerasa
 
     end
   end
