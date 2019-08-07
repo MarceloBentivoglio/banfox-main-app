4fbe1e61f48416c088cc95755dc360a52cee55f1
Ricardo Sasaki <sasaki@MacBook-Air-de-Ricardo.local>
2019-08-07 18:15:23 -0300

    adding safe navigation for a possible nil

--- app/models/risk/referee/founded_in.rb
+++ app/models/risk/referee/founded_in.rb
@@ -7,7 +7,7 @@ module Risk
         }
         @code = 'founded_in'
         @title = 'Founded In'
-        @description = @evidence[:founded_in].strftime
+        @description = @evidence[:founded_in]&.strftime
         @params = {
           yellow_limit: 3,
           red_limit: 2
