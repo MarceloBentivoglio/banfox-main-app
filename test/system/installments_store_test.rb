b1eba5b45c358740170ed32f6b8d3601e949740b
Furuchooo <furucho@banfox.com.br>
2019-08-13 14:12:13 -0300

    Added test to sign a partially approved operation with checking account select

--- test/system/installments_store_test.rb
+++ test/system/installments_store_test.rb
@@ -52,6 +52,35 @@ class InstallmentsStoreTest < ApplicationSystemTestCase
     assert_selector "a#opened-installments-button", text: "Ver status da minha operação"
   end
 
+  test 'sign a partially approved operation' do
+    using_shared_operation
+    ignore_slack_call
+    FactoryBot.create(:checking_account, seller: @seller)
+    @operation.installments.each do |installment|
+      installment.approved!
+    end
+    @operation.installments.first.rejected!
+
+    mocked_doc_info = {"signer_signature_keys"=>
+                        [
+                          {"email"=>"joaquim@banfox.com.br", "signature_key"=>"c1bfec78-fdf4-4e13-8793-a89c24ed9d01"},
+                          {"email"=>"joaquim.oliveira.nt@gmail.com", "signature_key"=>"edc94fa1-964c-4341-9e30-bb854dba47a1"},
+                          {"email"=>"joao@banfox.com.br", "signature_key"=>"17f110da-2491-402a-bbd3-7ddc3bbd9b7d"}
+                        ]
+                      }
+    mocked_doc_key = "198900ef-021f-4371-8373-ac3699f3667d"
+    mocked_document = mock()
+    mocked_document.expects(:sign_document_info).returns(mocked_doc_info)
+    mocked_document.expects(:sign_document_key).returns(mocked_doc_key)
+    SignDocuments.stubs(:new).returns(mocked_document)
+    sign_in FactoryBot.create(:user, seller: @seller)
+    visit store_installments_path
+    select "044", from: 'operation_checking_account_id'
+    click_button :'partially-approved-create-document-button'
+    assert_selector "span.section-title", text: "Contrato da operação"
+    assert_selector "a#opened-installments-button", text: "Ver status da minha operação"
+  end
+
   test 'cancel an operation in analysis' do
     using_shared_operation
     ignore_slack_call
