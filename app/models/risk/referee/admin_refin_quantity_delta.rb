03fda9a7059af7546c2dcdb530b5c600fc5cd81e
Ricardo Sasaki <sasaki@MacBook-Air-de-Ricardo.local>
2019-08-13 17:35:56 -0300

    added new admin referees

---
+++ app/models/risk/referee/admin_refin_quantity_delta.rb
@@ -0,0 +1,48 @@
+module Risk
+  module Referee
+    class AdminRefinQuantityDelta < Base
+      include DeltaEvaluator
+
+      def initialize(evidence)
+        @collection_evidence = evidence.admin_data
+        @code = 'admin_refin_quantity_delta'
+        @title = 'Admin Refin Quantity Delta'
+        @description = ''
+        @params = { green_limit: 0, yellow_limit: 0.5 }
+      end
+
+      def call
+        @collection_evidence.map do |evidence|
+          @evidence = {
+            historic: evidence.refin_historic_quantity,
+            current: evidence.refin_quantity,
+            cpf: evidence.partner_cpf,
+            name: evidence.partner_name
+          }
+
+          code = "#{@code}_#{evidence.partner_cpf}"
+
+          {
+            code: code,
+            title: @title,
+            description: evidence.partner_cpf,
+            params: @params,
+            evidence: @evidence,
+            flag: assert
+          }
+        end
+      end
+
+      def assert
+        evaluate_delta_for_negative_information(
+          @evidence[:historic],
+          @evidence[:current]
+        )
+      end
+
+      def multiple_assertions?
+        true
+      end
+    end
+  end
+end
