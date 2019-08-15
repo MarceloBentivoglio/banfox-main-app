57370fbe01295b142d235d427dbb99c5801daa30
Ricardo Sasaki <sasaki@MacBook-Air-de-Ricardo.local>
2019-08-15 17:10:02 -0300

    using a partner_document method to get either a cpf or a cnpj

--- test/models/risk/decorator/serasa_test.rb
+++ test/models/risk/decorator/serasa_test.rb
@@ -20,4 +20,23 @@ class Risk::Decorator::SerasaTest < ActiveSupport::TestCase
     assert_equal 0, decorator.refin_value
     assert_equal 1000, decorator.refin_historic_value
   end
+
+  test '.partner_document' do
+    sample_data = [
+      {
+        cpf: '11111111111'
+      },
+      {
+        cnpj: '22222222'
+      }
+    ]
+
+    expected_document = '11111111111'
+    decorator = Risk::Decorator::Serasa.new(sample_data.first)
+    assert_equal expected_document, decorator.partner_document
+
+    expected_document = '22222222'
+    decorator = Risk::Decorator::Serasa.new(sample_data.last)
+    assert_equal expected_document, decorator.partner_document
+  end
 end
