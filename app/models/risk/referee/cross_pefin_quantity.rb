975050f20bf59ba291a0e516ca1b7cdbd51c05db
Ricardo Sasaki <sasaki@MacBook-Air-de-Ricardo.local>
2019-08-20 15:52:44 -0300

    added cross_pefin_quantity referee

---
+++ app/models/risk/referee/cross_pefin_quantity.rb
@@ -0,0 +1,41 @@
+module Risk
+  module Referee
+    class CrossPefinQuantity < Base
+      include DeltaEvaluator
+      def initialize(evidence)
+        @root_evidence = evidence.with_indifferent_access
+        @evidence = {
+          pefin_value_delta: @root_evidence[:pefin_value_delta],
+          pefin_quantity_delta: @root_evidence[:pefin_quantity_delta]
+        }.with_indifferent_access
+
+        @code = 'cross_pefin_quantity'
+        @title = 'Cross Pefin Quantity'
+        @description = ''
+        @params = {
+          green_limit: 0.1
+        }
+      end
+
+      def assert
+        if @evidence[:pefin_quantity_delta]['flag'] == Risk::KeyIndicatorReport::RED_FLAG
+          return Risk::KeyIndicatorReport::YELLOW_FLAG 
+        elsif @evidence[:pefin_quantity_delta]['flag'] == Risk::KeyIndicatorReport::GREEN_FLAG
+          return Risk::KeyIndicatorReport::GREEN_FLAG
+        else
+          value_delta = delta(
+            @evidence.dig(:pefin_value_delta, :evidence, :historic_quantity),
+            @evidence.dig(:pefin_value_delta, :evidence, :current_quantity)
+          )
+
+          if value_delta <= @params[:green_limit]
+            @evidence[:pefin_quantity_delta]['ignored'] = true
+            Risk::KeyIndicatorReport::GREEN_FLAG
+          else
+            Risk::KeyIndicatorReport::YELLOW_FLAG
+          end
+        end
+      end
+    end
+  end
+end
