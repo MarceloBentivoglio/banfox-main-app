b24f28a1ac1d62e6618b9a33d253d60c6445c67b
Ricardo Sasaki <sasaki@MacBook-Air-de-Ricardo.local>
2019-08-21 16:03:17 -0300

    added cross refin referees

--- app/models/risk/pipeline/cross_serasa_indicators.rb
+++ app/models/risk/pipeline/cross_serasa_indicators.rb
@@ -2,7 +2,9 @@ module Risk
   module Pipeline
     class CrossSerasaIndicators < Base
       run_referees Risk::Referee::CrossPefinValue,
-                   Risk::Referee::CrossPefinQuantity
+                   Risk::Referee::CrossPefinQuantity,
+                   Risk::Referee::CrossRefinValue,
+                   Risk::Referee::CrossRefinQuantity
 
       def decorate_evidences(key_indicator_report)
         Risk::Decorator::KeyIndicatorReport.new(key_indicator_report)
