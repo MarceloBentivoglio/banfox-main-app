28690677f5af210ed9d58a9871e044a5e05517de
Joaquim Oliveira <joaquim.oliveira.nt@gmail.com>
2019-09-27 13:44:08 -0300

    fixing the display of balance in the store view

--- app/javascript/components/operationTotalValues.js
+++ app/javascript/components/operationTotalValues.js
@@ -26,7 +26,7 @@ const operationTotalValuesAccordingToCheck = () => {
   // });
   $("[data-installment]").change((event) => {
     // console.log( event);
-    const credit = $('#credit').data('credit');
+    const credit = Number($('#credit').data('credit'));
     const installments = $("[data-installment]:checked").toArray().map(el => JSON.parse(el.dataset.installment));
     const total = installments.reduce((sum, installment) => sum + installment.value_cents, 0);
     const fee = installments.reduce((sum, installment) => sum + Number(installment.fee.fractional), 0);
@@ -39,7 +39,7 @@ const operationTotalValuesAccordingToCheck = () => {
 const operationInAnalysisTotalValues = () => {
   document.addEventListener("DOMContentLoaded", (event) => {
     if (document.getElementById("status_bottom_bar") && (document.querySelectorAll('[data-installment]').length === 0)) {
-      const credit = $('#credit').data('credit');
+      const credit = Number($('#credit').data('credit'));
       const total = $(".installment_value").toArray().reduce((sum, installment) => sum + formatToNumber(installment.textContent), 0);
       const fee = $(".fee").toArray().reduce((sum, installment) => sum + formatToNumber(installment.textContent), 0);
       const net_value = $(".net_value").toArray().reduce((sum, installment) => sum + formatToNumber(installment.textContent), 0);
