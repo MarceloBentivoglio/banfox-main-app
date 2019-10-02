4ea9900e09df81654110e271cfe3fa2ffd135d03
banfox-furucho <furucho@banfox.com.br>
2019-10-02 18:36:24 -0300

    Changed header names on installments opened/store to fit the new document. Changed operation_content params to send to document_app

--- app/services/sign_documents/d4_sign.rb
+++ app/services/sign_documents/d4_sign.rb
@@ -143,8 +143,8 @@ class D4Sign
       gross_value: @operation.total_value_approved.format(symbol: ''),
       fee: @operation.initial_fee.format(symbol: ''),
       net_value: @operation.initial_net_value.format(symbol: ''),
-      first_deposit: @operation.initial_deposit_today.format(symbol: ''),
-      protection: @operation.initial_protection.format(symbol: ''),
+      deposit_today: @operation.deposit_today.format(symbol: ''),
+      used_balance: @operation.used_balance.format(symbol: ''),
     }
   end
 
@@ -164,8 +164,6 @@ class D4Sign
           value: installment.value.format(symbol: ''),
           fee: installment.initial_fee.format(symbol: ''),
           net_value: installment.initial_net_value.format(symbol: ''),
-          first_deposit: installment.first_deposit_amount.format(symbol: ''),
-          protection: installment.initial_protection.format(symbol: ''),
         }
         installments << i
       end
