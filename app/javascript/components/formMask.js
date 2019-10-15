68de691af8ac69e6d50115ac369368899fd62d63
Ricardo Sasaki <sasaki@MacBook-Air-de-Ricardo.local>
2019-10-15 15:57:50 -0300

    only show valid marks after validation occurs

--- app/javascript/components/formMask.js
+++ app/javascript/components/formMask.js
@@ -7,7 +7,9 @@ const currencyOptions = {
         fixed: true,
         fractionDigits: 2,
         decimalSeparator: ',',
-        thousandsSeparator: '.'
+        thousandsSeparator: '.',
+        reverse: true,
+        mask: '#.##0,00'
 };
 
 function maskCpfCnpjCurrency() {
@@ -18,15 +20,20 @@ function maskCpfCnpjCurrency() {
   $('.birth_date').mask('00/00/0000');
   $('.zip_code').mask('00000-000');
   if (document.getElementById("monthly_revenue")) {
-    SimpleMaskMoney.setMask('#monthly_revenue', currencyOptions);
-    SimpleMaskMoney.setMask('#monthly_fixed_cost', currencyOptions);
-    SimpleMaskMoney.setMask('#cost_per_unit', currencyOptions);
-    SimpleMaskMoney.setMask('#debt', currencyOptions);
+   SimpleMaskMoney.setMask('#monthly_revenue', currencyOptions);
+   SimpleMaskMoney.setMask('#monthly_fixed_cost', currencyOptions);
+   SimpleMaskMoney.setMask('#cost_per_unit', currencyOptions);
+   SimpleMaskMoney.setMask('#debt', currencyOptions);
+
+   if(document.getElementsByClassName('is-invalid').length == 0) {
+     let elements = document.getElementsByClassName('is-valid'),
+     elements_number = elements.length;
+
+     for(let i = 0; i < elements_number; i++) {
+       elements[0].classList.remove('is-valid');
+     }
+   }
   }
-  $("#monthly_revenue").mask("#.##0,00", {reverse: true, maxlength: false});
-  $("#monthly_fixed_cost").mask("#.##0,00", {reverse: true, maxlength: false});
-  $("#cost_per_unit").mask("#.##0,00", {reverse: true, maxlength: false});
-  $("#debt").mask("#.##0,00", {reverse: true, maxlength: false});
 }
 
 export { maskCpfCnpjCurrency };
