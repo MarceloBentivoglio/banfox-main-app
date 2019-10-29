1f55947ecf9ab78fcbf28c61b7886e4aa2a046d7
Ricardo Sasaki <sasaki@MacBook-Air-de-Ricardo.local>
2019-10-29 19:11:38 -0300

    Routing to a new_cnpj kind when historic is not found

--- app/models/risk/service/key_indicator_report_request.rb
+++ app/models/risk/service/key_indicator_report_request.rb
@@ -3,8 +3,7 @@ module Risk
     class KeyIndicatorReportRequest 
       def self.call(params)
         if params.keys.member?(:cnpjs) &&
-           params.keys.member?(:operation_id) &&
-           params.keys.member?(:kind)
+           params.keys.member?(:operation_id)
 
           if params[:cnpjs].nil?
             operation = Operation.find(params[:operation_id])
@@ -19,16 +18,19 @@ module Risk
 
       def call(params)
         request = Risk::KeyIndicatorReportRequest.create(operation_id: params[:operation_id],
-                                                         input_data: params[:cnpjs],
-                                                         kind: params[:kind]
+                                                         input_data: params[:cnpjs]
                                                         )
-        
+
         reports = params[:cnpjs].map do |cnpj|
+          cnpj = CNPJ.new(cnpj).stripped
+          historic = historic_of_last_operation(cnpj)
+          kind = historic.nil? ? 'new_cnpj' : 'recurrent_operation'
           Risk::KeyIndicatorReport.create(cnpj: cnpj,
                                           key_indicator_report_request_id: request.id,
-                                          kind: params[:kind],
+                                          kind: kind,
                                           ttl: DateTime.now + 7.days,
-                                          operation_id: params[:operation_id])
+                                          operation_id: params[:operation_id],
+                                          historic: JSON.generate(historic))
         end
 
         reports.each do |report|
@@ -36,6 +38,34 @@ module Risk
         end
 
         request
+      end
+
+      def historic_of_last_operation(cnpj)
+        shortened_cnpj = cnpj[0..7]
+        last_operation = Operation.joins(:installments, invoices: [:payer, :seller])
+                                    .where('installments.backoffice_status': ['approved' , 'deposited'])
+                                    .where('invoices.seller': identify_part(cnpj)&.id)
+                                    .where('sellers.id=? OR payers.id=?', identify_part(cnpj)&.id, identify_part(cnpj)&.id)
+                                    .last
+
+        last_operation&.key_indicator_reports
+                      &.select {|kir| kir.cnpj == cnpj }
+                      &.first
+                      &.evidences
+                      &.dig('serasa_api', shortened_cnpj)
+                      &.select {|evidence| !evidence.nil?}
+      end
+
+      def identify_part(cnpj)
+        sellers = Seller.where(cnpj: cnpj)
+        if sellers.any?
+          return sellers.first
+        end
+
+        payers = Payer.where(cnpj: cnpj)
+        return payers.first if payers.any?
+
+        nil
       end
 
       def self.validate_input_data(params)
