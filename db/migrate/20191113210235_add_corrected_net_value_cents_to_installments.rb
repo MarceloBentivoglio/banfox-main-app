e0578df20596e5ac09c75b54cd2be427f3b610de
Ricardo Sasaki <sasaki@MacBook-Air-de-Ricardo.local>
2019-11-13 18:45:29 -0300

    fixing timed migrations

--- db/migrate/20191113210235_add_corrected_net_value_cents_to_installments.rb
+++ db/migrate/20191113210235_add_corrected_net_value_cents_to_installments.rb
@@ -1,7 +1,7 @@
 class AddCorrectedNetValueCentsToInstallments < ActiveRecord::Migration[5.2]
   def change
     change_table :installments do |t|
-      t.bigint :corrected_net_value_cents
+      t.bigint :corrected_net_value_cents unless column_exists?(:installments, :corrected_net_value_cents)
     end
   end
 end
