ddea59bd72a72d3c813c6897d9f07ece2278f84a
banfox-furucho <furucho@banfox.com.br>
2019-11-20 15:03:11 -0300

    finished slack message on the nps service

--- app/services/net_promoter_score_service.rb
+++ app/services/net_promoter_score_service.rb
@@ -3,9 +3,21 @@ class NetPromoterScoreService
     seller = feedback.user&.seller
     questions_text = ""
     feedback.questions.each do |question|
-      questions_text += "\n #{question["question"]}: #{question["answer"]}"
+      if question["question"] == "Se a Banfox deixasse de existir, como você se sentiria?"
+        case question["answer"]
+        when "0"
+          question["answer"] = "Muito preocupado"
+        when "1"
+          question["answer"] = "Preocupado"
+        when "2"
+          question["answer"] = "Indiferente"
+        when "3"
+          question["answer"] = "Não sei dizer"
+        end
+      end
+      questions_text += "\n *#{question["question"]}* #{question["answer"]}"
     end
-    message = "Rating: #{feedback.rating} \n Recomendaria a Banfox? #{feedback.recommend_banfox} #{questions_text}"
-    SlackMessage.new("CQPBSJBPS", "<!channel> #{seller.company_name} envio o seguinte feedback: \n #{message}")
+    message = "*Rating* #{feedback.rating} \n *Recomendaria a Banfox?* #{feedback.recommend_banfox} #{questions_text}"
+    SlackMessage.new("CQPBSJBPS", "<!channel> #{seller&.company_name} envio o seguinte feedback: \n #{message}").send_now
   end
 end
